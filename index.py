560ee7150269c39c4d968579f791df189cf69d47
Carlos Vinícius <carlos.martins@cwi.com.br>
2023-11-16 19:46:17 -0300

    subindo imagens

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ for i in range(7, 60):
     if i == 59:
         df = df.rename(columns={f'Unnamed: {i}': 'Total'})
     else:
-        df = df.rename(columns={f'Unnamed: {i}': f'Semana_{semana}'})
+        df = df.rename(columns={f'Unnamed: {i}': f'S{semana}'})
         semana += 1
 linha_sp = df.loc[df['Regiao'] == 'SÃO PAULO']
 semanas_infectados = pd.DataFrame()
@@ -26,7 +26,7 @@ beta = 0.375 #taxa de contágio
 gama = 1/6
 
 for i in range(1, 53):
-    semanas_infectados[f'Semana_{i}'] = linha_sp[f'Semana_{i}']
+    semanas_infectados[f'S{i}'] = linha_sp[f'S{i}']
 
   #número de habitantes
 infectados = np.array(semanas_infectados.values.tolist()).flatten().tolist()
@@ -40,14 +40,16 @@ valores_sucetiveis_normalizados = scaler.fit_transform([[v] for v in sucetiveis]
 valores_infectados_normalizados = scaler.fit_transform([[v] for v in infectados])
 valores_recuperados_normalizados = scaler.fit_transform([[v] for v in recuperados])
 
+#Gráfico separado
+'''
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(8, 12))
 
 ax1.plot(semanas_infectados.columns.tolist(), sucetiveis, label="Sucetíveis", color='green', marker='.')
-ax1.set_ylabel('Infectados')
+ax1.set_ylabel('Suscetíveis')
 ax1.legend()
 
 ax2.plot(semanas_infectados.columns.tolist(), infectados, label="Infectados", color='red', marker='.')
-ax2.set_ylabel('Sucetiveis')
+ax2.set_ylabel('Infectados')
 ax2.legend()
 
 ax3.plot(semanas_infectados.columns.tolist(), recuperados, label="Recuperados", color='blue', marker='.')
@@ -56,10 +58,15 @@ ax3.set_ylabel('Recuperados')
 ax3.legend()
 
 fig.suptitle('Modelo Epidemiológico - Sucetíveis, Infectados e Recuperados')
+'''
 
-#plt.plot(semanas_infectados.columns.tolist(), valores_sucetiveis_normalizados, label="Sucetíveis", color='green', marker='.')
-#plt.plot(semanas_infectados.columns.tolist(), valores_infectados_normalizados, label="Infectados", color='red', marker='.')
+#Gráfico com todas as linhas juntas
+#plt.plot(semanas_infectados.columns.tolist(), sucetiveis, label="Sucetíveis", color='green', marker='.')
+plt.plot(semanas_infectados.columns.tolist(), infectados, label="Infectados", color='red', marker='.')
 #plt.plot(semanas_infectados.columns.tolist(), valores_recuperados_normalizados, label="Recuperados", color='blue', marker='.')
+plt.xlabel('Semanas')
+plt.ylabel('Pessoas')
+plt.title('Gráfico de pessoas infectadas ao longo do tempo')
 plt.xticks(rotation=45, ha='right')
 mplcursors.cursor(hover=True)
 
